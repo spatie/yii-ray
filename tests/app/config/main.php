6211e15644266bff6a646dfa971893ff46dc7fe2
Rias <hey@rias.be>
2021-01-14 13:34:09 +0100

    Don't need Mysql (for now)

--- tests/app/config/main.php
+++ tests/app/config/main.php
@@ -7,20 +7,6 @@ $config = [
     'bootstrap' => [
     ],
     'components' => [
-        'mysql' => [
-            'class' => \yii\db\Connection::class,
-            'dsn' => sprintf(
-                'mysql:host=%s;dbname=%s',
-                getenv('MYSQL_HOST') ?: 'localhost',
-                getenv('MYSQL_DATABASE') ?: 'yii2_queue_test'
-            ),
-            'username' => getenv('MYSQL_USER') ?: 'root',
-            'password' => getenv('MYSQL_PASSWORD') ?: '',
-            'charset' => 'utf8',
-            'attributes' => [
-                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET sql_mode = "STRICT_ALL_TABLES"',
-            ],
-        ],
     ],
 ];
 
