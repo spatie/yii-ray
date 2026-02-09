c7752ae9cef8f8a805245af845ed5ac5907fb66c
freekmurze <483853+freekmurze@users.noreply.github.com>
2026-02-09 20:12:02 +0000

    Fix styling

--- tests/app/config/main.php
+++ tests/app/config/main.php
@@ -1,9 +1,10 @@
 <?php
+
 $config = [
     'id' => 'yii2-ray-app',
     'basePath' => dirname(__DIR__),
-    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
-    'runtimePath' => dirname(dirname(__DIR__)) . '/runtime',
+    'vendorPath' => dirname(dirname(__DIR__)).'/vendor',
+    'runtimePath' => dirname(dirname(__DIR__)).'/runtime',
     'bootstrap' => [
     ],
     'components' => [
